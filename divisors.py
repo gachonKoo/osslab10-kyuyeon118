b5da0d2bd8bbd6864ad7f61aaa7e7927027d1193
kyuyeon <joymaker1523@gmail.com>
2025-11-09 17:52:02 +0900

    fix output format for autograder

--- divisors.py
+++ divisors.py
@@ -1,10 +1,14 @@
+import sys
+
 def divisors(n):
     result = []
-    for i in range(1, n + 1):
+    for i in range(1, n+1):
         if n % i == 0:
             result.append(i)
     return result
 
 if __name__ == "__main__":
-    num = int(input())
-    print(*divisors(num))
+    n = int(sys.argv[1])
+    result = divisors(n)
+
+    print(" ".join(str(x) for x in result))
